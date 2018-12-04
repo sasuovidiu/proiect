7fc4ce0e32eabd741a945d114362fa52d836012f
sasuovidiu <45397808+sasuovidiu@users.noreply.github.com>
2018-12-04 17:54:07 +0200

    Add files via upload

---
+++ sinth.js
@@ -0,0 +1,11 @@
+document.getElementById("id_logic_version").innerHTML="Logic version 2018.12.04.1
+var synth = window.speechSynthesis;
+function afiseaza_voci()
+{
+		var synth = window.speechSynthesis;
+		var lista_voci = synth.getVoices();
+		
+		for (var i = 0; i < lista_voci.length; i++)  
+			document.getElementById("id_voci").innerHTML+=
+				lista_voci[i].lang+" "+lista_voci[i].name +"<br>";
+}	
