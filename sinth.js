7dad63f7eecafa5ee4f5e13f4ecf047221bf20d2
sasuovidiu <45397808+sasuovidiu@users.noreply.github.com>
2018-12-04 18:46:16 +0200

    Add files via upload

--- sinth.js
+++ sinth.js
@@ -17,5 +17,11 @@ function vorbeste()
 	var enunt = new SpeechSynthesisUtterance();
 	enunt.text = document.getElementById("id_text").value;
 	enunt.lang = "en-US";
+	enunt.onend = sfarsit_vorbeste;
+	document.getElementById("id_buton_vorbeste").disabled = true;
 	synth.speak(enunt);
 }	
+//------------------------------------
+function sfarsit_vorbeste()
+{
+	document.getElementById("id_buton_vorbeste").disabled = false;
