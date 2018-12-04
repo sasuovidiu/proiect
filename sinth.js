ab4dcaf254a680642aed1fc2fb999ae04c92c7e9
sasuovidiu <45397808+sasuovidiu@users.noreply.github.com>
2018-12-04 18:35:23 +0200

    Add files via upload

--- sinth.js
+++ sinth.js
@@ -1,18 +1,21 @@
-document.getElementById("id_logic_version").innerHTML="Logic version 2018.12.04.1";
+document.getElementById("id_logic_version").innerHTML="Logic version 2018.12.04.4";
 var synth = window.speechSynthesis;
 
 //-----------------------------------
 function afiseaza_voci()
 {
+		document.getElementById("id_voci").innerHTML ="";
 		var lista_voci = synth.getVoices();
 		
 		for (var i = 0; i < lista_voci.length; i++)  
 			document.getElementById("id_voci").innerHTML+=
 				lista_voci[i].lang+" "+lista_voci[i].name +"<br>";
-}	//-------------------------------
+}	
+//------------------------------------
 function vorbeste()
 {
-	var enunt = new speechSynthesisUtterance();
-	enunt.text = document.getElementById("id_text"). value
+	var enunt = new SpeechSynthesisUtterance();
+	enunt.text = document.getElementById("id_text").value;
 	enunt.lang = "en-US";
 	synth.speak(enunt);
+}	
