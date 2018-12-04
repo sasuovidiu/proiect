5151b00fa3ede5ca1069d2b090ff54c92cae2729
sasuovidiu <45397808+sasuovidiu@users.noreply.github.com>
2018-12-04 18:24:20 +0200

    Add files via upload

--- synth.js
+++ synth.js
@@ -1,13 +1,14 @@
-document.getElementById("id_logic_version").innerHTML="Logic version 2018.12.04.1";
+document.getElementById("id_logic_version").innerHTML="Logic version 2018.12.04.4;
 var synth = window.speechSynthesis;
 
 //-----------------------------------
 function afiseaza_voci()
 {
+	document.getElementById("id_voci").innerHTML+= "";
 		var lista_voci = synth.getVoices();
 		
 		for (var i = 0; i < lista_voci.length; i++)  
-			document.getElementById("id_voci").innerHTML+=
+			
 				lista_voci[i].lang+" "+lista_voci[i].name +"<br>";
 }	//-------------------------------
 function vorbeste()
